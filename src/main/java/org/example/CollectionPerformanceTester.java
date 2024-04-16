c21754490180c1b6ea43e9cae1deab9218535ba5
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-16 14:24:47 +0300

    constructor is implemented

--- src/main/java/org/example/CollectionPerformanceTester.java
+++ src/main/java/org/example/CollectionPerformanceTester.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.util.List;
+
 public class CollectionPerformanceTester {
+    private List<Integer> list;
+    private long elapsedTime;
+
+    public CollectionPerformanceTester(List<Integer> list) {
+        this.list = list;
+    }
 }
