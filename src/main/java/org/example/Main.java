d8e22edf9aaa94cbd75f2d18ff08ecb295ba81ba
Alexander Potemkin <potemkin_av7@yandex.ru>
2024-04-16 16:18:28 +0300

    main fun is implemented

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int numIterations = 1000;
+        int numElements = 10000;
+
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> linkedList = new LinkedList<>();
+
+        CollectionPerformanceTester arrayListTester = new CollectionPerformanceTester(arrayList);
+        CollectionPerformanceTester linkedListTester = new CollectionPerformanceTester(linkedList);
+
+        System.out.println("Add method total execution time (ms):\n" +
+                "Number of iterations: " + numIterations);
+        System.out.println("ArrayList: " + arrayListTester.testAdd(numElements, numIterations) + " ms");
+        System.out.println("LinkedList: " + linkedListTester.testAdd(numElements, numIterations) + " ms");
+
+        System.out.println("\nDelete total method execution time (ms):\n" +
+                "Number of iterations: " + numIterations);
+        System.out.println("ArrayList: " + arrayListTester.testDelete(numElements, numIterations) + " ms");
+        System.out.println("LinkedList: " + linkedListTester.testDelete(numElements, numIterations) + " ms");
+
+        numIterations = 200;
+        System.out.println("\nGet method total execution time (ms):\n" +
+                "Number of iterations: " + numIterations);
+        System.out.println("ArrayList: " + arrayListTester.testGet(numElements, numIterations) + " ms");
+        System.out.println("LinkedList: " + linkedListTester.testGet(numElements, numIterations) + " ms");
     }
 }
